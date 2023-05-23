16674e6dd287d62e9330400bd4ce9e641b01b81b
edward.huang <edward.huang@backgardon.com>
2023-05-23 18:49:00 +0800

    vectory screen

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,6 +1,13 @@
 extends Node
 
+@export var vectory_screen: PackedScene
+
 @onready var timer = $Timer
 
 func get_time_elapsed():
 	return timer.wait_time - timer.time_left
+
+
+func _on_timer_timeout():
+	var vectory_screen_instance = vectory_screen.instantiate()
+	add_child(vectory_screen_instance)
